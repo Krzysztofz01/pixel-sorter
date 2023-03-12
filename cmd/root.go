1f8da4cc0a13648915e859262c312ff466cc4d13
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2023-03-12 10:09:23 +0100

    Enh: Downscaling entrypoint flag implemented

--- cmd/root.go
+++ cmd/root.go
@@ -24,6 +24,7 @@ var (
 	FlagMask                   bool
 	FlagIntervalLength         int
 	FlagSortCycles             int
+	FlagImageScale             float64
 	FlagVerboseLogging         bool
 )
 
@@ -66,6 +67,8 @@ func init() {
 	rootCmd.PersistentFlags().IntVarP(&FlagIntervalLength, "interval-max-length", "k", 0, "The max length of the interval. Zero means no length limits.")
 
 	rootCmd.PersistentFlags().IntVarP(&FlagSortCycles, "cycles", "c", 1, "The count of sorting cycles that should be performed on the image.")
+
+	rootCmd.PersistentFlags().Float64VarP(&FlagImageScale, "scale", "s", 1, "Image downscaling percentage factor. Options: [0.0 - 1.0].")
 }
 
 // Helper function used to validate and apply flag values into the sorter options struct
@@ -153,6 +156,12 @@ func parseCommonOptions() (*sorter.SorterOptions, error) {
 		return nil, fmt.Errorf("invalid cycles count specified")
 	} else {
 		options.Cycles = FlagSortCycles
+	}
+
+	if FlagImageScale < 0.0 || FlagImageScale > 1.0 {
+		return nil, fmt.Errorf("invalid image scale percentage specified")
+	} else {
+		options.Scale = FlagImageScale
 	}
 
 	return options, nil
