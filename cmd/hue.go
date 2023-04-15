ad1cfddae46634284282145443e09d0b063db2d5
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2023-04-15 14:47:38 +0200

    Enh: Subcmd boilerplate code moved to root cmd

--- cmd/hue.go
+++ cmd/hue.go
@@ -1,12 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-	"image"
-	"strings"
-
 	"github.com/Krzysztofz01/pixel-sorter/pkg/sorter"
-	"github.com/Krzysztofz01/pixel-sorter/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -22,44 +17,7 @@ var hueCmd = &cobra.Command{
 		}
 
 		options.SortDeterminant = sorter.SortByHue
-
-		if len(FlagImageFilePath) == 0 {
-			return fmt.Errorf("invalid image path specified: %q", FlagImageFilePath)
-		}
-
-		format := strings.ToLower(FlagOutputFileType)
-		if format != "jpg" && format != "png" {
-			return fmt.Errorf("invalid output file format specified: %q", FlagOutputFileType)
-		}
-
-		img, err := utils.GetImageFromFile(FlagImageFilePath)
-		if err != nil {
-			return err
-		}
-
-		var mask image.Image = nil
-		if len(FlagMaskFilePath) > 0 {
-			mask, err = utils.GetImageFromFile(FlagMaskFilePath)
-			if err != nil {
-				return err
-			}
-		}
-
-		sorter, err := sorter.CreateSorter(img, mask, options)
-		if err != nil {
-			return err
-		}
-
-		sortedImage, err := sorter.Sort()
-		if err != nil {
-			return err
-		}
-
-		if err := utils.StoreImageToFile(getOutputFileName(FlagImageFilePath), format, sortedImage); err != nil {
-			return err
-		}
-
-		return nil
+		return performPixelSorting(options)
 	},
 }
 
