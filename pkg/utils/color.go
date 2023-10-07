97fb29b3e25bb9a07e4669b4191b1a9cd806ac66
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2023-10-07 21:21:55 +0200

    Enh: Implementation of color space specific color utils and tests

--- pkg/utils/color.go
+++ pkg/utils/color.go
@@ -13,7 +13,16 @@ const (
 	DarkenOnly
 )
 
+// Convert the color.NRGBA color to the Y grayscale component represented as a integer in range from 0 to 255.
+// Note that the alpha channel is not taken under account and the result is non-alpha-premultiplied.
+func NrgbaToGrayscaleComponent(c color.NRGBA) int {
+	y := (float64(c.R) * 0.299) + (float64(c.G) * 0.587) + (float64(c.B) * 0.114)
+	return int(math.Max(0, math.Min(255, y)))
+}
+
 // Convert the color.Color interface instance to the Y grayscale component represented as a integer in range from 0 to 255
+//
+// Deprecated: NrgbaToGrayscaleComponent
 func ColorToGrayscaleComponent(c color.Color) int {
 	rgba := ColorToRgba(c)
 
@@ -31,6 +40,8 @@ func RgbaToNormalizedComponents(c color.RGBA) (float64, float64, float64) {
 }
 
 // Return a boolean value indicating if the given color.Color interface implementation has the alpha channel <255
+//
+// Deprecated
 func HasAnyTransparency(c color.Color) bool {
 	_, _, _, a32 := c.RGBA()
 
@@ -39,6 +50,8 @@ func HasAnyTransparency(c color.Color) bool {
 
 // Convert a color represented as color.Color interface to color.RGBA struct. If the underlying color is a color.RGBA the original struct
 // will be returned, otherwise a new color.RGBA instance will be created
+//
+// Deprecated
 func ColorToRgba(c color.Color) color.RGBA {
 	if rgba, ok := c.(color.RGBA); ok {
 		return rgba
@@ -53,8 +66,55 @@ func ColorToRgba(c color.Color) color.RGBA {
 	}
 }
 
+// Convert a color.RGBA color to HSL+Alpha components where Hue is expressed in degress (0-360) and the saturation, lightnes and alpha\
+// in percentage (0.0-1.0)
+func RgbaToHsla(c color.RGBA) (int, float64, float64, float64) {
+	rNorm, gNorm, bNorm := RgbaToNormalizedComponents(c)
+
+	alpha := float64(c.A) / 255.0
+
+	min := math.Min(rNorm, math.Min(gNorm, bNorm))
+	max := math.Max(rNorm, math.Max(gNorm, bNorm))
+	delta := max - min
+
+	lightness := (max + min) / 2.0
+	saturation := 0.0
+	hue := 0
+
+	if delta != 0.0 {
+		if lightness <= 0.5 {
+			saturation = delta / (max + min)
+		} else {
+			saturation = delta / (2.0 - max - min)
+		}
+
+		hueNorm := 0.0
+		if max == rNorm {
+			hueNorm = ((gNorm - bNorm) / 6.0) / delta
+		} else if max == gNorm {
+			hueNorm = (1.0 / 3.0) + ((bNorm-rNorm)/6.0)/delta
+		} else {
+			hueNorm = (2.0 / 3.0) + ((rNorm-gNorm)/6.0)/delta
+		}
+
+		if hueNorm < 0.0 {
+			hueNorm += 1.0
+		}
+
+		if hueNorm > 1.0 {
+			hueNorm -= 1.0
+		}
+
+		hue = int(math.Round(hueNorm * 360))
+	}
+
+	return hue, saturation, lightness, alpha
+}
+
 // Convert a color represented as color.Color interface implementation to HSL+Alpha components where Hue is expressed in degress (0-360) and the
 // saturation, lightnes and alpha in percentage (0.0-1.0)
+//
+// Deprecated: RgbaToHsla
 func ColorToHsla(c color.Color) (int, float64, float64, float64) {
 	rgba := ColorToRgba(c)
 	rNorm, gNorm, bNorm := RgbaToNormalizedComponents(rgba)
@@ -101,6 +161,8 @@ func ColorToHsla(c color.Color) (int, float64, float64, float64) {
 
 // Perform blending of two colors according to a given blending mode
 // TODO: Currently the alpha channel of the output color has a 0xff fixed value
+//
+// Deprecated: BlendNrgba
 func BlendRGBA(a, b color.RGBA, mode BlendingMode) color.RGBA {
 	switch mode {
 	case LightenOnly:
@@ -124,7 +186,34 @@ func BlendRGBA(a, b color.RGBA, mode BlendingMode) color.RGBA {
 	}
 }
 
+// Perform blending of two color.NRGBA colors according to a given blending mode
+// TODO: Currently the alpha channel of the output color has a fixed 0xff value
+func BlendNrgba(a, b color.NRGBA, mode BlendingMode) color.NRGBA {
+	switch mode {
+	case LightenOnly:
+		{
+			r := uint8(math.Max(float64(a.R), float64(b.R)))
+			g := uint8(math.Max(float64(a.G), float64(b.G)))
+			b := uint8(math.Max(float64(a.B), float64(b.B)))
+
+			return color.NRGBA{r, g, b, 0xff}
+		}
+	case DarkenOnly:
+		{
+			r := uint8(math.Min(float64(a.R), float64(b.R)))
+			g := uint8(math.Min(float64(a.G), float64(b.G)))
+			b := uint8(math.Min(float64(a.B), float64(b.B)))
+
+			return color.NRGBA{r, g, b, 0xff}
+		}
+	default:
+		panic("color-utils: undefined blending mode provided")
+	}
+}
+
 // Perform a RGBA-based linear interpolation between two colors and return the interpolated color.RGBA for the specified point
+//
+// Deprecated: InterpolateRgba
 func InterpolateColor(a, b color.Color, t float64) color.RGBA {
 	aRgba := ColorToRgba(a)
 	bRgba := ColorToRgba(b)
@@ -133,6 +222,21 @@ func InterpolateColor(a, b color.Color, t float64) color.RGBA {
 	gLerp := Lerp(float64(aRgba.G), float64(bRgba.G), t)
 	bLerp := Lerp(float64(aRgba.B), float64(bRgba.B), t)
 	aLerp := Lerp(float64(aRgba.A), float64(bRgba.A), t)
+
+	return color.RGBA{
+		R: uint8(rLerp),
+		G: uint8(gLerp),
+		B: uint8(bLerp),
+		A: uint8(aLerp),
+	}
+}
+
+// Performa a linear interpolation between two color.RGBA colors and return the interpolated color for the given t point
+func InterpolateRgba(a, b color.RGBA, t float64) color.RGBA {
+	rLerp := Lerp(float64(a.R), float64(b.R), t)
+	gLerp := Lerp(float64(a.G), float64(b.G), t)
+	bLerp := Lerp(float64(a.B), float64(b.B), t)
+	aLerp := Lerp(float64(a.A), float64(b.A), t)
 
 	return color.RGBA{
 		R: uint8(rLerp),
--- pkg/utils/color_test.go
+++ pkg/utils/color_test.go
@@ -205,3 +205,124 @@ func TestInterpolateColorShouldCorrectlyInterpolateColors(t *testing.T) {
 		assert.Equal(t, expected, actual)
 	}
 }
+
+func TestNrgbaToGrayscaleComponentShouldConvert(t *testing.T) {
+	cases := map[color.NRGBA]int{
+		{0, 0, 0, 0}:         0,
+		{0, 0, 0, 255}:       0,
+		{255, 255, 255, 0}:   255, // 0 if premultiplied
+		{255, 255, 255, 255}: 255,
+		{50, 100, 200, 0}:    96, // 0 if premultuplied
+		{50, 100, 200, 255}:  96,
+	}
+
+	for color, expected := range cases {
+		actual := NrgbaToGrayscaleComponent(color)
+
+		assert.Equal(t, expected, actual)
+	}
+}
+
+func TestRgbaToHslaShouldConvert(t *testing.T) {
+	cases := map[color.RGBA]struct {
+		h int
+		s float64
+		l float64
+		a float64
+	}{
+		{0, 0, 0, 0}:         {0, 0.0, 0.0, 0.0},
+		{0, 0, 0, 255}:       {0, 0.0, 0.0, 1.0},
+		{255, 255, 255, 0}:   {0, 0.0, 1.0, 0.0},
+		{255, 255, 255, 255}: {0, 0.0, 1.0, 1.0},
+		{255, 0, 0, 0}:       {0, 1.0, 0.5, 0.0},
+		{255, 0, 0, 255}:     {0, 1.0, 0.5, 1.0},
+		{50, 100, 200, 0}:    {220, 0.60, 0.49, 0.0},
+		{50, 100, 200, 255}:  {220, 0.60, 0.49, 1.0},
+		{55, 99, 7, 255}:     {89, 0.87, 0.21, 1.0},
+		{255, 250, 250, 0}:   {0, 1.0, 0.99, 0.0},
+		{255, 250, 250, 255}: {0, 1.0, 0.99, 1.0},
+		{255, 255, 0, 0}:     {60, 1.0, 0.5, 0.0},
+		{255, 255, 0, 255}:   {60, 1.0, 0.5, 1.0},
+	}
+
+	const deltaHue float64 = 1.0
+	const deltaSaturation float64 = 1e-2
+	const deltaLightness float64 = 1e-2
+	const deltaAlpha float64 = 1e-2
+
+	for color, expected := range cases {
+		hActual, sActual, lActual, aActual := RgbaToHsla(color)
+
+		if expected.s > 0.0 {
+			assert.InDelta(t, expected.h, hActual, deltaHue)
+		}
+
+		assert.InDelta(t, expected.s, sActual, deltaSaturation)
+		assert.InDelta(t, expected.l, lActual, deltaLightness)
+		assert.InDelta(t, expected.a, aActual, deltaAlpha)
+	}
+}
+
+func TestBlendNrgbaShouldBlendColorsUsingLightenOnlyMode(t *testing.T) {
+	cases := map[struct {
+		a color.NRGBA
+		b color.NRGBA
+	}]color.NRGBA{
+		{color.NRGBA{0, 0, 0, 0xff}, color.NRGBA{0, 0, 0, 0xff}}:             {0, 0, 0, 0xff},
+		{color.NRGBA{255, 255, 255, 0xff}, color.NRGBA{255, 255, 255, 0xff}}: {255, 255, 255, 0xff},
+		{color.NRGBA{0, 0, 0, 0xff}, color.NRGBA{255, 255, 255, 0xff}}:       {255, 255, 255, 0xff},
+		{color.NRGBA{25, 50, 200, 0xff}, color.NRGBA{200, 40, 20, 0xff}}:     {200, 50, 200, 0xff},
+	}
+
+	for c, exptected := range cases {
+		actual := BlendNrgba(c.a, c.b, LightenOnly)
+
+		assert.Equal(t, exptected, actual)
+	}
+}
+
+func TestBlendNrgbaShouldPanicForInvalidBlendingMode(t *testing.T) {
+	assert.Panics(t, func() {
+		c := color.NRGBA{}
+		BlendNrgba(c, c, -1)
+	})
+}
+
+func TestBlendNrgbaShouldBlendColorsUsingDarkenOnlyMode(t *testing.T) {
+	cases := map[struct {
+		a color.NRGBA
+		b color.NRGBA
+	}]color.NRGBA{
+		{color.NRGBA{0, 0, 0, 0xff}, color.NRGBA{0, 0, 0, 0xff}}:             {0, 0, 0, 0xff},
+		{color.NRGBA{255, 255, 255, 0xff}, color.NRGBA{255, 255, 255, 0xff}}: {255, 255, 255, 0xff},
+		{color.NRGBA{0, 0, 0, 0xff}, color.NRGBA{255, 255, 255, 0xff}}:       {0, 0, 0, 0xff},
+		{color.NRGBA{25, 50, 200, 0xff}, color.NRGBA{200, 40, 20, 0xff}}:     {25, 40, 20, 0xff},
+	}
+
+	for c, exptected := range cases {
+		actual := BlendNrgba(c.a, c.b, DarkenOnly)
+
+		assert.Equal(t, exptected, actual)
+	}
+}
+
+func TestInterpolateRgbaShouldCorrectlyInterpolateColors(t *testing.T) {
+	cases := map[struct {
+		a color.RGBA
+		b color.RGBA
+		t float64
+	}]color.Color{
+		{color.RGBA{0, 0, 0, 0xff}, color.RGBA{255, 255, 255, 0xff}, 0.5}: color.RGBA{127, 127, 127, 0xff},
+		{color.RGBA{255, 0, 0, 0xff}, color.RGBA{0, 0, 255, 0xff}, 0.0}:   color.RGBA{255, 0, 0, 0xff},
+		{color.RGBA{255, 0, 0, 0xff}, color.RGBA{0, 0, 255, 0xff}, 0.25}:  color.RGBA{191, 0, 63, 0xff},
+		{color.RGBA{255, 0, 0, 0xff}, color.RGBA{0, 0, 255, 0xff}, 0.5}:   color.RGBA{127, 0, 127, 0xff},
+		{color.RGBA{255, 0, 0, 0xff}, color.RGBA{0, 0, 255, 0xff}, 0.75}:  color.RGBA{63, 0, 191, 0xff},
+		{color.RGBA{255, 0, 0, 0xff}, color.RGBA{0, 0, 255, 0xff}, 1.0}:   color.RGBA{0, 0, 255, 0xff},
+	}
+
+	for c, expected := range cases {
+		actual := InterpolateRgba(c.a, c.b, c.t)
+
+		assert.Equal(t, expected, actual)
+	}
+}
