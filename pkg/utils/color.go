e9bb553b8c6034eb0a681e6cca4403d7702c73fe
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2023-11-01 20:46:03 +0100

    Enh: Deprecated image and color utils removed

--- pkg/utils/color.go
+++ pkg/utils/color.go
@@ -20,16 +20,6 @@ func NrgbaToGrayscaleComponent(c color.NRGBA) int {
 	return int(math.Max(0, math.Min(255, y)))
 }
 
-// Convert the color.Color interface instance to the Y grayscale component represented as a integer in range from 0 to 255
-//
-// Deprecated: NrgbaToGrayscaleComponent
-func ColorToGrayscaleComponent(c color.Color) int {
-	rgba := ColorToRgba(c)
-
-	y := (float64(rgba.R) * 0.299) + (float64(rgba.G) * 0.587) + (float64(rgba.B) * 0.114)
-	return int(math.Max(0, math.Min(255, y)))
-}
-
 // Convert the color.RGBA struct tu individual RGB components represented as floating point numbers in range from 0.0 to 1.0
 func RgbaToNormalizedComponents(c color.RGBA) (float64, float64, float64) {
 	rNorm := float64(c.R) / 255.0
@@ -39,19 +29,10 @@ func RgbaToNormalizedComponents(c color.RGBA) (float64, float64, float64) {
 	return rNorm, gNorm, bNorm
 }
 
-// Return a boolean value indicating if the given color.Color interface implementation has the alpha channel <255
-//
-// Deprecated
-func HasAnyTransparency(c color.Color) bool {
-	_, _, _, a32 := c.RGBA()
-
-	return int(a32>>8) < 255
-}
-
 // Convert a color represented as color.Color interface to color.RGBA struct. If the underlying color is a color.RGBA the original struct
 // will be returned, otherwise a new color.RGBA instance will be created
 //
-// Deprecated
+// Deprecated: This function is used by the mask feature which is still using the old color.Color API. This function can be removed after the mask feature refactor.
 func ColorToRgba(c color.Color) color.RGBA {
 	if rgba, ok := c.(color.RGBA); ok {
 		return rgba
@@ -111,81 +92,6 @@ func RgbaToHsla(c color.RGBA) (int, float64, float64, float64) {
 	return hue, saturation, lightness, alpha
 }
 
-// Convert a color represented as color.Color interface implementation to HSL+Alpha components where Hue is expressed in degress (0-360) and the
-// saturation, lightnes and alpha in percentage (0.0-1.0)
-//
-// Deprecated: RgbaToHsla
-func ColorToHsla(c color.Color) (int, float64, float64, float64) {
-	rgba := ColorToRgba(c)
-	rNorm, gNorm, bNorm := RgbaToNormalizedComponents(rgba)
-
-	alpha := float64(rgba.A) / 255.0
-
-	min := math.Min(rNorm, math.Min(gNorm, bNorm))
-	max := math.Max(rNorm, math.Max(gNorm, bNorm))
-	delta := max - min
-
-	lightness := (max + min) / 2.0
-	saturation := 0.0
-	hue := 0
-
-	if delta != 0.0 {
-		if lightness <= 0.5 {
-			saturation = delta / (max + min)
-		} else {
-			saturation = delta / (2.0 - max - min)
-		}
-
-		hueNorm := 0.0
-		if max == rNorm {
-			hueNorm = ((gNorm - bNorm) / 6.0) / delta
-		} else if max == gNorm {
-			hueNorm = (1.0 / 3.0) + ((bNorm-rNorm)/6.0)/delta
-		} else {
-			hueNorm = (2.0 / 3.0) + ((rNorm-gNorm)/6.0)/delta
-		}
-
-		if hueNorm < 0.0 {
-			hueNorm += 1.0
-		}
-
-		if hueNorm > 1.0 {
-			hueNorm -= 1.0
-		}
-
-		hue = int(math.Round(hueNorm * 360))
-	}
-
-	return hue, saturation, lightness, alpha
-}
-
-// Perform blending of two colors according to a given blending mode
-// TODO: Currently the alpha channel of the output color has a 0xff fixed value
-//
-// Deprecated: BlendNrgba
-func BlendRGBA(a, b color.RGBA, mode BlendingMode) color.RGBA {
-	switch mode {
-	case LightenOnly:
-		{
-			r := uint8(math.Max(float64(a.R), float64(b.R)))
-			g := uint8(math.Max(float64(a.G), float64(b.G)))
-			b := uint8(math.Max(float64(a.B), float64(b.B)))
-
-			return color.RGBA{r, g, b, 0xff}
-		}
-	case DarkenOnly:
-		{
-			r := uint8(math.Min(float64(a.R), float64(b.R)))
-			g := uint8(math.Min(float64(a.G), float64(b.G)))
-			b := uint8(math.Min(float64(a.B), float64(b.B)))
-
-			return color.RGBA{r, g, b, 0xff}
-		}
-	default:
-		panic("color-utils: undefined blending mode provided")
-	}
-}
-
 // Perform blending of two color.NRGBA colors according to a given blending mode
 // TODO: Currently the alpha channel of the output color has a fixed 0xff value
 func BlendNrgba(a, b color.NRGBA, mode BlendingMode) color.NRGBA {
@@ -208,26 +114,6 @@ func BlendNrgba(a, b color.NRGBA, mode BlendingMode) color.NRGBA {
 		}
 	default:
 		panic("color-utils: undefined blending mode provided")
-	}
-}
-
-// Perform a RGBA-based linear interpolation between two colors and return the interpolated color.RGBA for the specified point
-//
-// Deprecated: InterpolateRgba
-func InterpolateColor(a, b color.Color, t float64) color.RGBA {
-	aRgba := ColorToRgba(a)
-	bRgba := ColorToRgba(b)
-
-	rLerp := Lerp(float64(aRgba.R), float64(bRgba.R), t)
-	gLerp := Lerp(float64(aRgba.G), float64(bRgba.G), t)
-	bLerp := Lerp(float64(aRgba.B), float64(bRgba.B), t)
-	aLerp := Lerp(float64(aRgba.A), float64(bRgba.A), t)
-
-	return color.RGBA{
-		R: uint8(rLerp),
-		G: uint8(gLerp),
-		B: uint8(bLerp),
-		A: uint8(aLerp),
 	}
 }
 
--- pkg/utils/image.go
+++ pkg/utils/image.go
@@ -2,202 +2,14 @@ package utils
 
 import (
 	"errors"
-	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
 	"math"
-	"sync"
 
 	"github.com/Krzysztofz01/pimit"
 	"github.com/disintegration/imaging"
 )
-
-// Get the image column as a color.Color interface implementation slice specified by the x image index. The retrieval
-// process is run parallel in several goroutines.
-func GetImageColumn(image image.Image, xIndex int) ([]color.Color, error) {
-	width := image.Bounds().Dx()
-	height := image.Bounds().Dy()
-
-	if xIndex >= width {
-		return nil, errors.New("image-utils: column index is not in range of the target image width")
-	}
-
-	iteratorCount := height / 400
-	if iteratorCount < 1 {
-		iteratorCount = 1
-	}
-
-	lengthBase := height / iteratorCount
-	lengthRemnant := height % iteratorCount
-
-	wg := sync.WaitGroup{}
-	wg.Add(iteratorCount)
-
-	column := make([]color.Color, height)
-	for offsetFactor := 0; offsetFactor < iteratorCount; offsetFactor += 1 {
-		targetOffset := offsetFactor * lengthBase
-		targetLength := lengthBase
-		if offsetFactor+1 == iteratorCount {
-			targetLength += lengthRemnant
-		}
-
-		go func(offset, length int) {
-			defer wg.Done()
-
-			for iterationOffset := 0; iterationOffset < length; iterationOffset += 1 {
-				currentOffset := offset + iterationOffset
-
-				color := image.At(xIndex, currentOffset)
-				column[currentOffset] = color
-			}
-		}(targetOffset, targetLength)
-	}
-
-	wg.Wait()
-	return column, nil
-}
-
-// Get the image row as a color.Color interface implementation slice specified by the y image index. The retrieval
-// process is run parallel in several goroutines.
-func GetImageRow(image image.Image, yIndex int) ([]color.Color, error) {
-	width := image.Bounds().Dx()
-	height := image.Bounds().Dy()
-
-	if yIndex >= height {
-		return nil, errors.New("image-utils: row index is not in range of the target image height")
-	}
-
-	iteratorCount := width / 400
-	if iteratorCount < 1 {
-		iteratorCount = 1
-	}
-
-	lengthBase := width / iteratorCount
-	lengthRemnant := width % iteratorCount
-
-	wg := sync.WaitGroup{}
-	wg.Add(iteratorCount)
-
-	row := make([]color.Color, width)
-	for offsetFactor := 0; offsetFactor < iteratorCount; offsetFactor += 1 {
-		targetOffset := offsetFactor * lengthBase
-		targetLength := lengthBase
-		if offsetFactor+1 == iteratorCount {
-			targetLength += lengthRemnant
-		}
-
-		go func(offset, length int) {
-			defer wg.Done()
-
-			for iterationOffset := 0; iterationOffset < length; iterationOffset += 1 {
-				currentOffset := offset + iterationOffset
-
-				color := image.At(currentOffset, yIndex)
-				row[currentOffset] = color
-			}
-		}(targetOffset, targetLength)
-	}
-
-	wg.Wait()
-	return row, nil
-}
-
-// Set the colors of the column of the given image specified by the xIndex, according to the slice containing implementations
-// of the color.Color interface. The changes made will be applied to the given draw.Image reference. The write process is
-// run parallel in several goroutines.
-func SetImageColumn(image draw.Image, column []color.Color, xIndex int) error {
-	width := image.Bounds().Dx()
-	height := image.Bounds().Dy()
-
-	if xIndex >= width {
-		return errors.New("image-utils: column index is not in range of the target image width")
-	}
-
-	if len(column) != height {
-		return errors.New("image-utils: the image height and the provided column lengths are not matching")
-	}
-
-	iteratorCount := height / 400
-	if iteratorCount < 1 {
-		iteratorCount = 1
-	}
-
-	lengthBase := height / iteratorCount
-	lengthRemnant := height % iteratorCount
-
-	wg := sync.WaitGroup{}
-	wg.Add(iteratorCount)
-
-	for offsetFactor := 0; offsetFactor < iteratorCount; offsetFactor += 1 {
-		targetOffset := offsetFactor * lengthBase
-		targetLength := lengthBase
-		if offsetFactor+1 == iteratorCount {
-			targetLength += lengthRemnant
-		}
-
-		go func(offset, length int) {
-			defer wg.Done()
-
-			for iterationOffset := 0; iterationOffset < length; iterationOffset += 1 {
-				currentOffset := offset + iterationOffset
-
-				image.Set(xIndex, currentOffset, column[currentOffset])
-			}
-		}(targetOffset, targetLength)
-	}
-
-	wg.Wait()
-	return nil
-}
-
-// Set the colors of the row of the given image specified by the yIndex, according to the slice containing implementations
-// of the color.Color interface. The changes made will be applied to the given draw.Image reference. The write process is
-// run parallel in several goroutines.
-func SetImageRow(image draw.Image, row []color.Color, yIndex int) error {
-	width := image.Bounds().Dx()
-	height := image.Bounds().Dy()
-
-	if yIndex >= height {
-		return errors.New("image-utils: row index is not in range of the target image height")
-	}
-
-	if len(row) != width {
-		return errors.New("image-utils: the image widht and the provided row lengths are not matching")
-	}
-
-	iteratorCount := width / 400
-	if iteratorCount < 1 {
-		iteratorCount = 1
-	}
-
-	lengthBase := width / iteratorCount
-	lengthRemnant := width % iteratorCount
-
-	wg := sync.WaitGroup{}
-	wg.Add(iteratorCount)
-
-	for offsetFactor := 0; offsetFactor < iteratorCount; offsetFactor += 1 {
-		targetOffset := offsetFactor * lengthBase
-		targetLength := lengthBase
-		if offsetFactor+1 == iteratorCount {
-			targetLength += lengthRemnant
-		}
-
-		go func(offset, length int) {
-			defer wg.Done()
-
-			for iterationOffset := 0; iterationOffset < length; iterationOffset += 1 {
-				currentOffset := offset + iterationOffset
-
-				image.Set(currentOffset, yIndex, row[currentOffset])
-			}
-		}(targetOffset, targetLength)
-	}
-
-	wg.Wait()
-	return nil
-}
 
 // Convert a image represented by image.Image to a NRGBA image. If the underlying type is already NRGBA a reference to the
 // original input will be returned, otherwis a copy will be created.
@@ -261,50 +73,6 @@ func GetImageCopyRgba(i *image.RGBA) *image.RGBA {
 	return img
 }
 
-// Get the drawable copy of the provided image. The image is also redrawn to a new image.RGBA struct
-//
-// Deprecated: GetImgageCopy...
-func GetDrawableImage(i image.Image) (draw.Image, error) {
-	p1 := image.Point{0, 0}
-	p2 := image.Point{i.Bounds().Dx(), i.Bounds().Dy()}
-
-	rgbaImage := image.NewRGBA(image.Rectangle{p1, p2})
-	draw.Draw(rgbaImage, rgbaImage.Bounds(), i, i.Bounds().Min, draw.Src)
-
-	// NOTE: As we always convert the image to the RGBA image which IS implementing the draw.Image
-	// this check becomes redundant. We may keep it here in case we drop the idea of redrawing the
-	// image for performance purposes.
-	//
-	// if _, ok := rgbaImage.(*draw.Image); !ok {
-	// 	return nil, errors.New("image-utils: unable to create a drawable image")
-	// }
-
-	return rgbaImage, nil
-}
-
-// Invert the colors (negative-effect) of the image
-//
-// Deprecated: InvertImageNrgba
-func InvertImage(i image.Image) (draw.Image, error) {
-	drawableImage, err := GetDrawableImage(i)
-	if err != nil {
-		return nil, fmt.Errorf("image-utils: can not get the drawable image version: %w", err)
-	}
-
-	pimit.ParallelColumnColorReadWrite(drawableImage, func(c color.Color) color.Color {
-		currentColor := ColorToRgba(c)
-
-		return color.RGBA{
-			R: 255 - currentColor.R,
-			G: 255 - currentColor.G,
-			B: 255 - currentColor.B,
-			A: currentColor.A,
-		}
-	})
-
-	return drawableImage, nil
-}
-
 // Invert the colors (negative-effect) of the given NRGBA image.
 func InvertImageNrgba(i *image.NRGBA) *image.NRGBA {
 	if i == nil {
@@ -314,25 +82,6 @@ func InvertImageNrgba(i *image.NRGBA) *image.NRGBA {
 	return pimit.ParallelNrgbaReadWriteNew(i, func(x, y int, r, g, b, a uint8) (uint8, uint8, uint8, uint8) {
 		return 255 - r, 255 - g, 255 - b, a
 	})
-}
-
-// Rotate the image by a given angle
-//
-// Beacuse the dependency internal rotate implementation is using a custom pixel color handling
-// solution we need to redraw the result image to ensure that the colors space is RGBA
-//
-// Deprecated: RotateImageNrgba
-func RotateImage(i image.Image, angle int) draw.Image {
-	angleNorm := float64(angle) + math.Ceil(-float64(angle)/360.0)*360.0
-
-	rotatedImage := imaging.Rotate(i, angleNorm, color.Transparent)
-	rotatedImageWidth := rotatedImage.Bounds().Dx()
-	rotatedImageHeight := rotatedImage.Bounds().Dy()
-
-	redrawnImage := image.NewRGBA(image.Rect(0, 0, rotatedImageWidth, rotatedImageHeight))
-	draw.Draw(redrawnImage, rotatedImage.Bounds(), rotatedImage, rotatedImage.Bounds().Min, draw.Src)
-
-	return redrawnImage
 }
 
 // Rotate the NRGBA image counter-clockwise by a given angle.
@@ -392,40 +141,6 @@ func RotateImageWithRevertNrgba(i *image.NRGBA, angle int) (*image.NRGBA, func(*
 	return rotated, revertFunc
 }
 
-// FIXME: Fix borders after interpolation and implement full transparency support in order to preserve PNG data.
-// FIXME: Better handling for situations where there is not requirement to trim the workspace. Currently in such
-// situation we are returning the original imageWithWorkspace draw.Image instead of creating a copy.
-//
-// Remove all excess transparent workspace created during the rotation process from the image.
-// The function is calculating the image located in the middle of the workspace and is cropping it.
-// Due to rotation interpolation the borders of the image are not matching the original image,
-// and the whole operation can cause some problems realted to transparent colors.
-//
-// Time complexity: O(n)
-//
-// Deprecated: RotateImageWithRevertNrgba
-func TrimImageTransparentWorkspace(imageWithWorkspace draw.Image, imageOriginal image.Image) draw.Image {
-	xIndexStart := (imageWithWorkspace.Bounds().Dx() - imageOriginal.Bounds().Dx()) / 2
-	xIndexLength := imageOriginal.Bounds().Dx()
-
-	yIndexStart := (imageWithWorkspace.Bounds().Dy() - imageOriginal.Bounds().Dy()) / 2
-	yIndexLength := imageOriginal.Bounds().Dy()
-
-	if xIndexLength == 0 && yIndexLength == 0 {
-		return imageWithWorkspace
-	}
-
-	tImg := image.NewRGBA(image.Rect(0, 0, xIndexLength, yIndexLength))
-	pimit.ParallelColumnReadWrite(tImg, func(xIndex, yIndex int, _ color.Color) color.Color {
-		xOffset := xIndex + xIndexStart
-		yOffset := yIndex + yIndexStart
-
-		return imageWithWorkspace.At(xOffset, yOffset)
-	})
-
-	return tImg
-}
-
 func trimImageTransparentWorkspaceNrgba(withWorkspace *image.NRGBA, original image.Rectangle) *image.NRGBA {
 	var (
 		originalWidth   int = original.Dx()
@@ -446,28 +161,6 @@ func trimImageTransparentWorkspaceNrgba(withWorkspace *image.NRGBA, original ima
 	return img
 }
 
-// Function used to scale the image down according to given percentage parameter (Value from 0.0 to 1.0)
-//
-// Deprecated: ScaleImageNrgba
-func ScaleImage(i image.Image, percentage float64) (draw.Image, error) {
-	if percentage < 0.0 || percentage > 1.0 {
-		return nil, errors.New("image-utils: invalid downscale percentage specified")
-	}
-
-	scaledWidth := float64(i.Bounds().Dx()) * percentage
-	scaledHeight := float64(i.Bounds().Dy()) * percentage
-
-	// NOTE: The usage of the "Lanczos" algorithm will produce images with better quality, for perfomance use "Box"
-	scaledImage := imaging.Resize(i, int(scaledWidth), int(scaledHeight), imaging.Lanczos)
-
-	scaledDrawableImage, err := GetDrawableImage(scaledImage)
-	if err != nil {
-		return nil, fmt.Errorf("image-utils: can not get the scaled drawable image version: %w", err)
-	}
-
-	return scaledDrawableImage, nil
-}
-
 // Function used to scale a NRGBA image down according to given percentage parameter (Value from 0.0 to 1.0).
 func ScaleImageNrgba(i *image.NRGBA, percentage float64) (*image.NRGBA, error) {
 	if percentage <= 0.0 || percentage > 1.0 {
@@ -483,45 +176,6 @@ func ScaleImageNrgba(i *image.NRGBA, percentage float64) (*image.NRGBA, error) {
 
 	// NOTE: The usage of the "Lanczos" algorithm will produce images with better quality, for perfomance use "Box"
 	return imaging.Resize(i, scaledWidth, scaledHeight, imaging.Lanczos), nil
-}
-
-// Blend two images using a given blending mode into a new image
-//
-// Deprecated: BlendImagesNrgba
-func BlendImages(a, b image.Image, mode BlendingMode) (draw.Image, error) {
-	aWidth := a.Bounds().Dx()
-	aHeight := a.Bounds().Dy()
-
-	bWidth := b.Bounds().Dx()
-	bHeight := b.Bounds().Dy()
-
-	if aWidth != bWidth {
-		return nil, errors.New("image-utils: the provided images have a different width")
-	}
-
-	if aHeight != bHeight {
-		return nil, errors.New("image-utils: the provided images have a different height")
-	}
-
-	aRgba, err := GetDrawableImage(a)
-	if err != nil {
-		return nil, fmt.Errorf("image-utils: can not get the drawable image version of a: %w", err)
-	}
-
-	bRgba, err := GetDrawableImage(b)
-	if err != nil {
-		return nil, fmt.Errorf("image-utils: can not get the drawable image version of b: %w", err)
-	}
-
-	resultImage := image.NewRGBA(image.Rect(0, 0, aWidth, aHeight))
-	pimit.ParallelColumnReadWrite(resultImage, func(xIndex, yIndex int, _ color.Color) color.Color {
-		aColor := ColorToRgba(aRgba.At(xIndex, yIndex))
-		bColor := ColorToRgba(bRgba.At(xIndex, yIndex))
-
-		return BlendRGBA(aColor, bColor, mode)
-	})
-
-	return resultImage, nil
 }
 
 // Blend two NRGBA images using a given blending mode into a new image.
